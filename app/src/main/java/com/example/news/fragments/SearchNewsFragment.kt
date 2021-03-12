6a0952945537a0864b8a897b3414cc019ee83cb1
yash <yashkumar201301@gmail.com>
2021-03-12 23:53:02 +0530

    search news

--- app/src/main/java/com/example/news/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/news/fragments/SearchNewsFragment.kt
@@ -2,19 +2,94 @@ package com.example.news.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.news.NewsActivity
 import com.example.news.R
+import com.example.news.adapters.NewsAdapter
+import com.example.news.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.example.news.util.Resource
 import com.example.news.viewmodels.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     private lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+
+        setupRec()
+
+        var job: Job? = null
+        editText.addTextChangedListener { editable ->
+
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                if (editable != null) {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+
+        }
+
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message.let {
+
+                    }
+                }
+
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+
+            }
+
+        })
+
     }
+
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRec() {
+        newsAdapter = NewsAdapter()
+        recyclerView.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
 
 }
