c1085edb54452f41765a17be1ce49e4ea6355e79
yash <yashkumar201301@gmail.com>
2021-03-12 23:38:52 +0530

    news fragment

--- app/src/main/java/com/example/news/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/news/viewmodels/NewsViewModel.kt
@@ -1,12 +1,40 @@
 package com.example.news.viewmodels
 
+import androidx.annotation.NavigationRes
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.news.models.NewsResponse
 import com.example.news.repository.NewsRepository
+import com.example.news.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
 
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+
+        return Resource.Error(response.message())
+    }
 
 }
